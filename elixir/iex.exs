c9630d00cf0f13fff1b8e6560136cab3a8af6623
Caleb <caleb.mcclain89@gmail.com>
2025-07-01 20:39:05 +0900

    Create iex.exs

---
+++ elixir/iex.exs
@@ -0,0 +1,27 @@
+IEx.configure(
+  colors: [
+    syntax_colors: [
+      number: :light_yellow,
+      atom: :light_cyan,
+      string: :light_black,
+      boolean: [:light_blue],
+      nil: [:magenta, :bright]
+    ],
+    ls_directory: :cyan,
+    ls_device: :yellow,
+    doc_code: :green,
+    doc_inline_code: :magenta,
+    doc_headings: [:cyan, :underline],
+    doc_title: [:cyan, :bright, :underline]
+  ],
+  default_prompt:
+    [
+      :light_magenta,
+      "🧪 iex",
+      ">",
+      :white,
+      :reset
+    ]
+    |> IO.ANSI.format()
+    |> IO.chardata_to_string()
+)
